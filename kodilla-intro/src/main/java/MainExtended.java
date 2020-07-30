210c2e78191f513508873572f44b90141e64f6ff
Radoslaw Pula <radoslawpula@gmail.com>
2020-07-30 19:01:59 +0200

    beta version

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String courseName = "Tester Automatyczny";
+        boolean imDoingFine = true;
+        boolean goodTask = true;
+        int currentYear = 2020;
+        double weight = 89.5;
+        char firstNameIni = 'R';
+        System.out.println(courseName);
+        System.out.println(imDoingFine);
+        System.out.println(goodTask);
+        System.out.println(currentYear);
+        System.out.println(weight);
+        System.out.println(firstNameIni);
+    }
+}
