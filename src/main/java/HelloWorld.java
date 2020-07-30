1738aa4114d0162957e54abd08f06d2c67039724
Radoslaw Pula <radoslawpula@gmail.com>
2020-07-30 18:30:37 +0200

    Initial version

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Radosław");
+        System.out.println(9);
+        System.out.println("P");
+    }
+}
