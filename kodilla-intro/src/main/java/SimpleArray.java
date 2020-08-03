c392eee5127b2331efd38233aaeb882f19ae776b
Radoslaw Pula <radoslawpula@gmail.com>
2020-08-03 21:10:39 +0200

    version 12

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,10 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] fruits = new String[5];
+        fruits[0] = "Apple";
+        fruits[1] = "Banana";
+        fruits[2] = "Peach";
+        fruits[3] = "Watermelon";
+        fruits[4] = "Strawberry";
+    }
+}
